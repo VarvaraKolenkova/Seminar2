fa60d3e01f9a6634793258bb9b1a465fa1e839c6
Varvara.Kolenkova <var.kolenkova@yandex.ru>
2022-07-17 08:40:19 +0300

    Показать вторую цифру в трехзначном числе

---
+++ Task1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите трехзначное число от 1 до 999");
+string number = Console.ReadLine();
+int a = int.Parse(number);
+int a1 = a%10;
+int a2 = a/10%10;
+Console.WriteLine(a2);
+
