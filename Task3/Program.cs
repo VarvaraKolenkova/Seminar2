f906eef3fb3eff5f0e095e96d877e6f7a7da9dbb
Varvara.Kolenkova <var.kolenkova@yandex.ru>
2022-07-17 09:28:00 +0300

    Определяем день недели

---
+++ Task3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите текущий день недели в формате от 1 до 7, где понедельник=1, воскресенье=7");
+string number = Console.ReadLine();
+int a = int.Parse(number);
+
+if (a==6 || a==7) Console.WriteLine("Сегодня выходной день");
+else Console.WriteLine("Сегодня будний день");
